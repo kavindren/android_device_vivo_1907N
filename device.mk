d147fd0cc98743371605b20cfc16366cea22acce
skibidisigma28 <82117204+kavindren@users.noreply.github.com>
2026-03-09 17:16:16 +0300

    Initial release (2)

---
+++ device.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := device/vivo/1907N
+PRODUCT_USE_DYNAMIC_PARTITIONS := false
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/dtb.img:$(PRODUCT_OUT)/dtb.img
+
+PRODUCT_PLATFORM := mt6768
+PRODUCT_BOARD := k68v1_64
+
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
